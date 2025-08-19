f9ff1eadf8e9b8e07a08e40f413984da2a806dd0
MetinAbbaszade <metinabbaszade@yahoo.com>
2025-08-19 15:49:35 +0400

    fix

--- server.js
+++ server.js
@@ -1,13 +1,8 @@
-// server.js
 const jsonServer = require("json-server");
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
-const middlewares = jsonServer.defaults({ static: "./fake-dir" }); // avoid looking for ./public
-
+const middlewares = jsonServer.defaults({ static: "./build" });
+const port = process.env.PORT || 3000;
 server.use(middlewares);
 server.use(router);
-
-const port = process.env.PORT || 3000;
-server.listen(port, () => {
-    console.log(`JSON Server is running on port ${port}`);
-});
+server.listen(port);
